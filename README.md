3b9bc6b60864bbb27f0e89f3a9dc03288d76dc1d
Semyon Grigorev <grigorev.semen2@yandex.ru>
2025-05-21 16:22:44 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Layout Perfect Pixel - Проект на Next.js
+
+Приложение на Next.js для реализации верстки макета (https://www.figma.com/design/ugDsET9AfdwJMCQ4qf2Qfg/Coca---Digital-Marketing-Website?node-id=2-8643&p=f&t=k7URxWld1LqeW9E0-0).
+
+## Используемые технологии:
+
+*   Next.js
+*   TypeScript
+*   Styled Components
+
+## Скрипты:
+
+*   `dev`: Запускает сервер разработки с Turbopack.
+*   `build`: Собирает приложение для продакшена.
+*   `start`: Запускает продакшен сервер.
+*   `lint`: Запускает ESLint для проверки качества кода и соблюдения стиля.
---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import typescriptEslintParser from '@typescript-eslint/parser';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
+import nextEslintPlugin from '@next/eslint-plugin-next';
+import prettierPlugin from 'eslint-plugin-prettier'; 
+
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptEslintParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        HTMLDivElement: 'readonly',
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
+      react: reactPlugin,
+      'prettier': prettierPlugin,
+      '@next/next': nextEslintPlugin,
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'no-extra-semi': 'warn',
+      'eqeqeq': 'error',
+      ...typescriptEslintPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...nextEslintPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error', 
+    },
+  },
+];
